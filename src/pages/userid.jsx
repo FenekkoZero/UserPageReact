c1d5aad9ba3284873b82ae66cc897538d617b9b9
FenekkoZero <ivan.almeida28@yahoo.com>
2025-10-25 21:48:46 -0500

    Fixed incorrect css import in user id page

--- src/pages/userid.jsx
+++ src/pages/userid.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import '../styles/userId.css'
+import '../styles/userid.css'
 import { Link } from "react-router"
 
 import profilePlaceholder from '../assets/profile_placeholder.svg'
